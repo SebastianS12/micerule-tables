67aecf6878b00c798422f2a58c9d78309b1e1dae
Sebastian <sesere@gmx.de>
2023-05-30 12:38:55 +0200

    added missing = for single entry class

--- public/partials/Event/FancierEntries.php
+++ public/partials/Event/FancierEntries.php
@@ -20,12 +20,12 @@ class FancierEntries {
       foreach($userRegistrationData->userRegistrations as $className => $ageRegistrationData){
         foreach($ageRegistrationData as $age => $registrationData){
           if($userRegistrationData->getUserClassRegistrationCount($className, $age) > 0){
-              $html .= "<p class 'single-entry'><span>".$eventClasses->getClassIndex($className, $age)." ".$className." ".$age.": </span><span>".$userRegistrationData->getUserClassRegistrationCount($className, $age)."</span></p>";
+              $html .= "<p class='single-entry'><span>".$eventClasses->getClassIndex($className, $age)." ".$className." ".$age.": </span><span>".$userRegistrationData->getUserClassRegistrationCount($className, $age)."</span></p>";
           }
         }
       }
       if($userRegistrationData->juvenileRegistrationCount > 0){
-        $html .= "<p class 'single-entry'><span>".$eventClasses->getClassIndex("juvenile", "AA")." Juvenile AA: </span><span>".$userRegistrationData->juvenileRegistrationCount."</span></p>";
+        $html .= "<p class='single-entry'><span>".$eventClasses->getClassIndex("juvenile", "AA")." Juvenile AA: </span><span>".$userRegistrationData->juvenileRegistrationCount."</span></p>";
       }
       $html .= "<p class='single-entry'><span>Total Entries:</span><span>".$userRegistrationData->getEntryCount()."</span></p>";
       $html .= "<p class='single-entry'><span>Prize Money:</span><span>£".number_format((float)$this->userPrizeData->getUserPrizeMoney($userName, $eventOptionalSettings->prizeMoney['firstPrize'], $eventOptionalSettings->prizeMoney['secondPrize'], $eventOptionalSettings->prizeMoney['thirdPrize']), 2, '.', '')."</span></p>";
