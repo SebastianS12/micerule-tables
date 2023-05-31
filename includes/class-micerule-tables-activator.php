f9742a8780ba4229937a56f7e41f1d5e031a7a53
Sebastian <sesere@gmx.de>
2023-05-31 20:06:06 +0200

    added sql database for breeds

--- includes/class-micerule-tables-activator.php
+++ includes/class-micerule-tables-activator.php
@@ -11,7 +11,8 @@ class Micerule_Tables_Activator {
 	public static function activate() {
 		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
 		global $wpdb;
-		global $charset_collate;
+		$charset_collate = $wpdb->get_charset_collate();
+
 		$mr_tables_name = $wpdb->prefix . "micerule_result_tables";
 		$sql_create_table = "CREATE TABLE " . $mr_tables_name . " (
 			mrtable_id bigint(20) unsigned NOT NULL auto_increment,
@@ -21,8 +22,19 @@ class Micerule_Tables_Activator {
 			seasonTable int,
 			PRIMARY KEY  (mrtable_id)
 			) $charset_collate; ";
-
 			dbDelta( $sql_create_table );
-		}
 
+		//breed table
+		$breed_table_name = $wpdb->prefix."micerule_breeds";
+		$sql_create_breed_table = "CREATE TABLE ".$breed_table_name."(
+			id bigint(20) unsigned NOT NULL auto_increment,
+			name text,
+			colour text,
+			css_class text,
+			section text,
+			path text,
+			PRIMARY KEY  (id),
+			) $charset_collate; ";
+			dbDelta($sql_create_breed_table);
+		}
 	}
