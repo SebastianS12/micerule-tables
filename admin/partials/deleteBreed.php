8453427a1f6bf850643c09521dbaeb90cf47a02e
Sebastian <sesere@gmx.de>
2023-06-05 15:16:41 +0200

    Reworked Delete Breed, deletes db entries now

--- admin/partials/deleteBreed.php
+++ admin/partials/deleteBreed.php
@@ -8,40 +8,9 @@
 global $wpdb;
 global $post;
 
-$name=$_POST['name'];
-$category = $_POST['category'];
-
-$option_name = "mrTables_".$name."_".$category;
-$where = array("option_name"=>$option_name);
-$table = $wpdb->prefix."options";
-
-//get id of option to be deleted and update id_option
-$id  = get_option($option_name)['id'];
-$ids = get_option("mrOption_id");
-
-//build new array without deleted option
-$idUpdate = array();
-foreach($ids as $key => $value){
-  if($key != $id){
-    $idUpdate[$key]=$value;
-  }
-}
-//update option with array without deleted option
-update_option("mrOption_id",$idUpdate);
-
-//delete path option
-$paths= get_option("mrOption_paths");
-$pathUpdate= array();
-foreach($paths as $key => $value){
-  if($key != $id){
-    $pathUpdate[$key]=$value;
-  }
-}
-update_option("mrOption_paths",$pathUpdate);
-
-//delete Breed Option
-delete_option($option_name);
-
-echo("success");
+$id = $_POST['id'];
+$table_name = $wpdb->prefix . "micerule_breeds";
+$where = array("id" => $id);
+$wpdb->delete($table_name, $where);
 
 wp_die();
