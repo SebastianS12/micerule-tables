2efbe4b2296de4d7903f90154b1de8966761b4b9
Sebastian <sesere@gmx.de>
2025-06-22 14:50:32 +0100

    Fixed #125

--- public/js/shortCuts.js
+++ public/js/shortCuts.js
@@ -1,43 +1,44 @@
 jQuery(document).ready(function($){
+    const headerHeight = $('#menu-header-custom').outerHeight();
     $("#show-prize-cards-btn").on('click', function(){
         showTab($(".prizeCards.tab"));
         $([document.documentElement, document.body]).animate({
-            scrollTop: $("#prizeCards-content").offset().top
+            scrollTop: $("#prizeCards-content").offset().top - headerHeight
         }, 2000);
     });
 
     $("#show-selfs-section-btn").on('click', function(){
         showTab($(".entryBook.tab"));
         $([document.documentElement, document.body]).animate({
-            scrollTop: $("#eb-section-selfs").offset().top
+            scrollTop: $("#eb-section-selfs").offset().top - headerHeight
         }, 2000);
     });
 
     $("#show-tans-section-btn").on('click', function(){
         showTab($(".entryBook.tab"));
         $([document.documentElement, document.body]).animate({
-            scrollTop: $("#eb-section-tans").offset().top
+            scrollTop: $("#eb-section-tans").offset().top - headerHeight
         }, 2000);
     });
 
     $("#show-marked-section-btn").on('click', function(){
         showTab($(".entryBook.tab"));
         $([document.documentElement, document.body]).animate({
-            scrollTop: $("#eb-section-marked").offset().top
+            scrollTop: $("#eb-section-marked").offset().top - headerHeight
         }, 2000);
     });
 
     $("#show-satins-section-btn").on('click', function(){
         showTab($(".entryBook.tab"));
         $([document.documentElement, document.body]).animate({
-            scrollTop: $("#eb-section-satins").offset().top
+            scrollTop: $("#eb-section-satins").offset().top - headerHeight
         }, 2000);
     });
 
     $("#show-aovs-section-btn").on('click', function(){
         showTab($(".entryBook.tab"));
         $([document.documentElement, document.body]).animate({
-            scrollTop: $("#eb-section-aovs").offset().top
+            scrollTop: $("#eb-section-aovs").offset().top - headerHeight
         }, 2000);
     });
 });
